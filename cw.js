3895ef5833dea9d146fd5dc81e416e90f429eb95
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-03-20 11:06:03 -0400

    Api requests,handle and display

--- cw.js
+++ cw.js
@@ -1,19 +1,36 @@
-async function sayJoke() {
-  // use mocked `fetch(url)`
-  const BASE_URL = "https://v2.jokeapi.dev/joke/Christmas";
+let obj = {};
+const BASE_URL = "https://v2.jokeapi.dev/joke/Christmas";
 
+async function sayJoke() {
   try {
-   const response = await fetch(BASE_URL).then((res) => {
-      return res.json();
-    });
-    return response
+    const response = await fetch(BASE_URL);
+    const data = response.json();
+    return data;
   } catch (error) {
     console.log("error!");
   }
 }
 
-async function getData(){
-  sayJoke().then(data=> console.log(data.id))
+async function getData() {
+  try {
+    const data = await sayJoke();
+    obj.id = data.id;
+    obj.setup = data.setup;
+    obj.delivery = data.delivery;
+
+    return obj;
+  } catch (error) {
+    console.log("error");
+  }
 }
 
-getData()
+getData();
+
+async function displayData() {
+  const joke = document.getElementsByTagName("h3");
+  const data = await getData();
+
+  joke[0].innerHTML = `${obj.setup} => ${obj.delivery}`;
+}
+
+displayData();
